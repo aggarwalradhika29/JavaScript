c0b9f70ec250ba714e94370f684cdaf98599a0ca
AggarwalRadhika29 <singlanorale17@gmail.com>
2023-07-18 23:02:45 +0530

    commit

--- index.js
+++ index.js
@@ -8,14 +8,35 @@
 // }
 // btn.addEventListener('click', onBtnClick);
 
-var mainHeading = document.getElementById("main-heading");
-var btn = document.getElementById('button-click');
-var sidebar = document.getElementById()
-mainHeading.classList.add('big');
+// var mainHeading = document.getElementById("main-heading");
+// var btn = document.getElementById('button-click');
+// var sidebar = document.getElementById()
+// mainHeading.classList.add('big');
 
-btn.addEventListener('click', function() {
-    mainHeading.classList.remove('big');
-});
+// btn.addEventListener('click', function() {
+//     mainHeading.classList.remove('big');
+// });
 // setTimeout(function() {
     
 // }, 1500);
+
+var username = document.getElementById('username');
+username.addEventListener('input', function(event) {
+    var currentValue = event.target.value;
+    currentValue = currentValue.toUpperCase();
+    console.log(currentValue);
+    username.value = currentValue;
+});
+
+username.addEventListener('focus', function() {
+    console.log("Element focused!");
+});
+username.addEventListener('blur', function() {
+    console.log('Lost focus!');
+});
+
+var loginForm = document.getElementById("login-form");
+loginForm.addEventListener('submit', function(e) {
+    alert('Submit button clicked!');
+    e.preventDefault();
+});
