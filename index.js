4746880a6ba09b33b572891ff994aa6d8c8ecee7
AggarwalRadhika29 <singlanorale17@gmail.com>
2023-07-18 21:31:31 +0530

    commit

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-var btn = document.getElementById("button-click");
-var mainDiv = document.querySelector("header div");
-// console.log(mainDiv);
-// console.log(btn);
-function onBtnClick() {
-    // alert("Button Clicked");
-    mainDiv.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) + ", " + Math.floor(Math.random() * 255) + ", " + Math.floor(Math.random() * 255) + ")";
-}
-btn.addEventListener('click', onBtnClick);
+// var btn = document.getElementById("button-click");
+// var mainDiv = document.querySelector("header div");
+// // console.log(mainDiv);
+// // console.log(btn);
+// function onBtnClick() {
+//     // alert("Button Clicked");
+//     mainDiv.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) + ", " + Math.floor(Math.random() * 255) + ", " + Math.floor(Math.random() * 255) + ")";
+// }
+// btn.addEventListener('click', onBtnClick);
+
+var mainHeading = document.getElementById("main-heading");
+var btn = document.getElementById('button-click');
+var sidebar = document.getElementById()
+mainHeading.classList.add('big');
+
+btn.addEventListener('click', function() {
+    mainHeading.classList.remove('big');
+});
+// setTimeout(function() {
+    
+// }, 1500);
